587472600b0a336158b03d431373b419bf2af832
James R. Davenport <jrdavenport85@gmail.com>
2016-03-12 13:01:24 +0000

    refine language field to remote extraneous text

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ app.get('/', function(req,res){
   
   var head = req.headers,
       ip = head["x-forwarded-for"],
-      lang = head["accept-language"],
+      lang = head["accept-language"]
+            .split(',')[0],
       ua = head["user-agent"]
             .match(/\(([^\)]+)\)/)[1]
-  
+  console.log(head)
   res.json({
     "ipaddress": ip, "language": lang, "software": ua
   }).end()
